2253fa79dcb2da9f2ba12a50688b2142c4e22f22
Jacob Albright <jacob.albright23@gmail.com>
2018-12-04 19:40:50 -0600

    wrote push, pop, shift, unshift, get methods for SinglyLinkedList class

--- data-structures/singly-linked-list.js
+++ data-structures/singly-linked-list.js
@@ -44,9 +44,94 @@ class SinglyLinkedList {
   }
 
   push(val) {
-    
+    const node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
     this.length += 1;
+    return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length -= 1;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length -= 1;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+
+  unshift(val) {
+    const newHead = new Node(val);
+    if (!this.head) {
+      this.head = newHead;
+      this.tail = this.head;
+    } else {
+      newHead.next = this.head;
+      this.head = newHead;
+    }
+    this.length += 1;
+    return this;
+  }
+
+  get(num) {
+    if (num < 0 || num >= this.length) return null;
+    let traverser = this.head;
+    for (let i = 0; i < num; i++) {
+      traverser = traverser.next;
+    }
+    return traverser;
+  }
+
 }
 
 const list = new SinglyLinkedList();
+// list.push('Hi');
+// list.push('How are ya??');
+// list.push(99);
+
+// console.log(list);
+list.unshift(84);
+// console.log(list);
+list.unshift(73);
+list.push(99);
+console.log(list.head.val);
+console.log(list.head.next.val);
+console.log(list.head.next.next.val);
+
+console.log(list.length);
+console.log(list.get(3));
+
+
+
+
+// console.log(list.head.val);
+// console.log(list.pop());
+// console.log(list.shift().val);
+// console.log(list.head.val);
+// list.unshift('mem eme me');
+// console.log(list.head);
