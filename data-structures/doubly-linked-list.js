f2fa4efe70d40b0a018581033c15df484985ee0c
Jacob Albright <jacob.albright23@gmail.com>
2018-12-19 21:09:19 -0600

    implemented a Stack class with push and pop methods

--- data-structures/doubly-linked-list.js
+++ data-structures/doubly-linked-list.js
@@ -11,6 +11,19 @@
   Compared to Signly Linked Lists
   - More flexible
   - Uses more memory
+
+  Big O of Doubly Linked Lists:
+    - Insertion O(1)
+    - Removal O(1)
+    - Searching O(n)
+    - Access O(n)
+
+  Recap:
+    - Doubly Linked Lists are almost identical to Singly Linked Lists except
+      there is an additional pointer (useful for something like browser history...)
+    - Better than Singly Linked Lists for finding nodes and can be done in half the time
+    - However, they do take up more memory considering the extra pointer
+
 */
 
 class Node {
--- data-structures/stack.js
+++ data-structures/stack.js
@@ -11,3 +11,80 @@
   - Routing (the history object) is treated like a stack
   - We'll use Stacks in Trees and Graphs
 */
+
+// THERE IS MORE THAN ONE WAY TO IMPLEMENT A STACK
+
+// Here it is with an Array...FIFO
+const arrayStack = [];
+arrayStack.push('gym');
+arrayStack.push('store');
+arrayStack.push('restaurant');
+arrayStack.pop();
+arrayStack.pop();
+arrayStack.push('bookstore');
+
+// could use shift() and unshift() but these would require 
+// all elements in the array to be re-indexed
+
+// HERE IS A CUSTOM IMPLEMENTATION
+// ...with a Linked List
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  toArr(full = false) {
+    const accum = [];
+    let current = this.first;
+    for (let i = 0; i < this.size; i++) {
+      const value = full ? current : current.val;
+      accum.push(value);
+      current = current.next;
+    }
+    return accum;
+  }
+
+
+  push(val) {
+    const newNode = new Node(val);
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+    this.size += 1;
+  }
+
+  pop() {
+    if (this.size === 0) return null;
+    const popped = this.first;
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = popped.next;
+    }
+    this.size -= 1;
+    return popped;
+  }
+}
+
+const stack = new Stack();
+stack.push(9);
+stack.push('hello');
+
+console.log(stack.toArr())
+console.log(stack);
+console.log(stack.pop());
+
